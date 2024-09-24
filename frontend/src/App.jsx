957780cd8bf89d33e1b87ffe3b3a25d6ee23ac40
Ntando <mngnta011@myuct.ac.za>
2024-09-24 10:39:06 +0200

    Started front-end with vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>My secure user authentication front-end</h1> 
+    </>
+  )
+}
+
+export default App
