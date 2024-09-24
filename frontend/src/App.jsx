8accd72ae3d0797860074ade4817364e783360d5
Ntando <mngnta011@myuct.ac.za>
2024-09-24 16:48:15 +0200

    Front-end signin design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,34 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import './App.css'
+import SignIn from './components/login/SignIn'
+import Register from './components/register/Register'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>My secure user authentication front-end</h1> 
-    </>
+    <Router>
+      <div className="home-container">
+      <nav className="navbar">
+    <div className="logo">PRODIGY_FS_01</div>
+    <div className="navbar-buttons">
+      
+      <Link to="/signin">
+        <button className="signin">Sign In</button>
+      </Link>
+      <Link to="/register">
+        <button className="register">Register</button>
+      </Link>
+    </div>
+  </nav>
+      </div>
+
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ frontend/src/components/login/SignIn.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import './SignIn.css';
+
+function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://127.0.0.1:5000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+      const data = await response.json();
+      if (data.message === 'Login successful') {
+        alert('You are logged in!');
+      } else {
+        alert(data.message);
+      }
+    } catch (error) {
+      console.error('Error during login:', error);
+    }
+  };
+
+  return (
+    <div className="signin-container">
+      <form className="signin-form" onSubmit={handleSubmit}>
+        <h2>Sign In</h2>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          required
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          required
+        />
+        <button type="submit">Sign In</button>
+      </form>
+    </div>
+  );
+}
+
+export default SignIn;
